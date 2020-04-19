791fec845cd963a85c2777c392ac09b148c222a1
Olga Jaeger <olgar.mamaeva@rambler.ru>
2020-04-19 16:00:08 +0200

    re-build

---
+++ build/precache-manifest.f8ec22994df6f6214324ae991f539f80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874bda290d0e1da546800ac27215a2bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "78c7cd02441d4ce140c3",
+    "url": "/static/js/2.29394c15.chunk.js"
+  },
+  {
+    "revision": "3a2e159706135df3ec8974df24e6376a",
+    "url": "/static/js/2.29394c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f35eec11afa82a3def",
+    "url": "/static/js/main.6b461d90.chunk.js"
+  },
+  {
+    "revision": "87d2a03ce04dcca3aac0",
+    "url": "/static/js/runtime-main.dd508654.js"
+  }
+]);
